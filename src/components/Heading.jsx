488a7e4d81f41080e26e0186a024e8fc778d34c0
fazza-tech <ebranfazza@gmail.com>
2025-04-24 13:55:32 +0530

    added every.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import logo from '../assets/swiggy-logo.svg'; 
 
 const Heading = () => {
 
@@ -7,7 +8,7 @@ const Heading = () => {
     return(
         <div className="header">
             <div className="logo-container">
-                <img className="logo" src="\src\assets\swiggy-logo.svg" alt="" />
+                <img className="logo" src={logo} alt="" />
                 <h4 className="location">Other Kozhikodu, Kerala, India</h4>
             </div>
             <div className="nav-items">
